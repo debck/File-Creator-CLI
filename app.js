e0b4ce377346214cd02ae78332f47d22c4e3321e
debck <debasish.sahoo1998@gmail.com>
2018-07-26 16:58:02 +0530

    initial work :tada:

---
+++ app.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+
+const inquirer = require("inquirer");
+const shell = require("shelljs");
+
+const init = require('./lib/initialize');
+
+const askQuestions = () => {
+  const questions = [
+    {
+      name: "FILENAME",
+      type: "input",
+      message: "What is the name of the file without extension?"
+    },
+    {
+      type: "list",
+      name: "EXTENSION",
+      message: "What is the file extension?",
+      choices: [".rb", ".js", ".php", ".css"],
+      filter: function(val) {
+        return val.split(".")[1];
+      }
+    }
+  ];
+  return inquirer.prompt(questions);
+};
+
+const createFile = (filename, extension) => {
+  const filePath = `${process.cwd()}/${filename}.${extension}`
+  shell.touch(filePath);
+  return filePath;
+};
+
+const success = filepath => {
+  console.log(
+    chalk.white.bgGreen.bold(`Done! File created at ${filepath}`)
+  );
+};
+
+const main = async () => {
+  // show script introduction
+  init.init();
+
+  // ask questions
+  const answers = await askQuestions();
+  const { FILENAME, EXTENSION } = answers;
+
+  // create the file
+  const filePath = createFile(FILENAME, EXTENSION);
+
+  // show success message
+  success(filePath);
+};
+
+main();
