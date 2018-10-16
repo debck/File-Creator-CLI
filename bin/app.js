fc229d0f4122585c206798a9050a628264678a6f
Autumn Smith <autumnsmith@Autumns-MacBook-Pro-2.local>
2018-10-15 22:56:41 -0700

    Fixed linting issues

--- bin/app.js
+++ bin/app.js
@@ -1,25 +1,22 @@
 #!/usr/bin/env node
 
-const shell = require("shelljs");
-const inquirer = require("inquirer");
-const chalk = require("chalk");
+const shell = require('shelljs');
+const inquirer = require('inquirer');
+const chalk = require('chalk');
 const header = require('../lib/header');
 const question = require('../lib/question');
 const create = require('../lib/create');
 
-
-
 const main = async () => {
-
   header.init();
 
   const input = await question.questions();
-  //console.log(input);
+  // console.log(input);
   const { filename, extension } = input;
-  //console.log(filename);
+  // console.log(filename);
   create.create(filename, extension);
 
-  console.log(chalk.blue(`File created successfully`)+chalk.red('!!!'));
-  }
+  console.log(chalk.blue('File created successfully') + chalk.red('!!!'));
+};
 
 main();
