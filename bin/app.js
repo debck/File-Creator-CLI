ec41cef0a2f4cfb2a012565352ed707c6f939c62
Layman806 <wakingbavani@gmail.com>
2019-01-14 12:51:28 +0530

    Added test cases, fixes (#7) and possibly, (#11)

--- bin/app.js
+++ bin/app.js
@@ -1,34 +1,4 @@
 #!/usr/bin/env node
+const sub = require('./sub.js');
 
-const shell = require('shelljs');
-const inquirer = require('inquirer');
-const chalk = require('chalk');
-const header = require('../lib/header');
-const question = require('../lib/question');
-const create = require('../lib/create');
-const ifdir = require('../lib/ifdir');
-
-const main = async () => {
-	header.init();
-
-	const result = await ifdir.ask();
-
-	const { isdir } = result;
-
-	if (isdir === 'd') {
-		const dir_res = await ifdir.getdir();
-		const { dirname } = dir_res;
-		create.createdir(dirname);
-
-		return;
-	} else {
-		const input = await question.questions();
-		// console.log(input);
-		const { filename, extension } = input;
-		// console.log(filename);
-		create.create(filename, extension);
-		console.log(chalk.blue('File created successfully') + chalk.red('!!!'));
-	}
-};
-
-main();
+sub.main();
