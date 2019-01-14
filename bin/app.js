57de9d316d807a3134bbe4b114bf919d87629166
Layman806 <wakingbavani@gmail.com>
2019-01-14 12:39:09 +0530

    Revert "Added test cases and fixed build, fixes (#7) and tries to fix (#11)"
    This reverts commit 54b8bb468a1726494afd92080d8ff75a5292430d.

--- bin/app.js
+++ bin/app.js
@@ -1,4 +1,34 @@
 #!/usr/bin/env node
-const sub = require('./sub.js');
 
-sub.main();
+const shell = require('shelljs');
+const inquirer = require('inquirer');
+const chalk = require('chalk');
+const header = require('../lib/header');
+const question = require('../lib/question');
+const create = require('../lib/create');
+const ifdir = require('../lib/ifdir');
+
+const main = async () => {
+	header.init();
+
+	const result = await ifdir.ask();
+
+	const { isdir } = result;
+
+	if (isdir === 'd') {
+		const dir_res = await ifdir.getdir();
+		const { dirname } = dir_res;
+		create.createdir(dirname);
+
+		return;
+	} else {
+		const input = await question.questions();
+		// console.log(input);
+		const { filename, extension } = input;
+		// console.log(filename);
+		create.create(filename, extension);
+		console.log(chalk.blue('File created successfully') + chalk.red('!!!'));
+	}
+};
+
+main();
